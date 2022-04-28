e05065567c7dddf66d9904915c0efc62a2cf25f1
divya36837 <dsri36837@gmail.com>
2022-04-28 09:44:40 +0530

    added features

---
+++ src/main/java/com/petshop/petshopbookingapi/PetshopbookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.petshopbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopbookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopbookingApiApplication.class, args);
+	}
+
+}
